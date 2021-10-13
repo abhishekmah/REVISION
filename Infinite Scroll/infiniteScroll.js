422cb909f3f089d50130e0d3065008ee3fb2d396
Abhishek Mahato <mahatoabhishek123@gmail.com>
2021-10-13 18:16:43 +0530

    Infinite Scroll

---
+++ Infinite Scroll/infiniteScroll.js
@@ -0,0 +1,28 @@
+var list = document.getElementById("main");
+var a = 1;
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF".split("");
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.round(Math.random() * 15)];
+  }
+  return color;
+}
+
+var listFunc = function () {
+  for (var i = 0; i < 25; i++) {
+    var color = getRandomColor();
+    var item = document.createElement("li");
+    item.innerText = "Masai Student " + a++;
+    item.style.color = color;
+    list.appendChild(item);
+  }
+};
+
+list.addEventListener("scroll", function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    listFunc();
+  }
+});
+listFunc();
